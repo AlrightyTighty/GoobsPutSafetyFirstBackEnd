d339d30c48eebb4eb43a3e47c9a65fb6b673f28b
AlrightyTighty <ileikpotatozzzz@gmail.com>
2025-04-04 12:15:04 -0500

    fix issues with getting flight info

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ API_KEY = "f6d1b4dbacc4354090a9504d91db369d" # change this to be an environment 
 
 app = Flask(__name__)
 
-@app.route("/flight-info", methods=['POST', 'GET'])
+@app.route("/flight-info", methods=['GET'])
 def get_flight_info():
-    flight_number = request.args.get('flightnum')
-    res = requests.get("https://api.aviationstack.com/v1/flights?access_key="+API_KEY+"&flight_number="+flight_number)
+    flight_code = request.args.get('flightcode')
+    res = requests.get("https://api.aviationstack.com/v1/flights?access_key="+API_KEY+"&flight_iata="+flight_code)
     print(res.json())
-    return "ok"
+    return res.json()['data'][0]
 
 if __name__ == "__main__":
     app.run(debug=True)
