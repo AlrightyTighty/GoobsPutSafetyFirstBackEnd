59ddb3a6f51e65f226f3a4c8db3d48c2120f050e
AlrightyTighty <ileikpotatozzzz@gmail.com>
2025-04-04 12:28:34 -0500

    added CORS support

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import requests
 import os
 
 API_KEY = os.getenv("FLIGHT_API_KEY") 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/flight-info", methods=['GET'])
+@cross_origin()
 def get_flight_info():
     flight_code = request.args.get('flightcode')
     res = requests.get("https://api.aviationstack.com/v1/flights?access_key="+API_KEY+"&flight_iata="+flight_code)
